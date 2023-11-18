d99add2e3b2745d70b764f0fff769e0b25774439
Jasmijn Bookelmann <jasmijn@bookelmann.nl>
2023-11-18 17:43:31 +0100

    add media and data folder

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,17 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+module.exports = function(config) {
+  config.addPlugin(eleventyNavigationPlugin);
+
+  config.addPassthroughCopy('media');
+  config.addPassthroughCopy('favicon.png');
+  config.addPassthroughCopy('favicon.svg');
 
   return {
     dir: {
       input: "site", 
-      includes: "includes"
+      includes: "includes",
+      data: "data",
     }
   };
 };
