8521f90f3b62e0ce049870f061373b24371001ba
Jasmijn Bookelmann <jasmijn@bookelmann.nl>
2023-11-18 17:44:21 +0100

    add tailwind and tailwind css file

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./site/**/*.njk'],
+  theme: {
+    extend: {},
+    colors: {
+      "bg" : "#0D0F15",
+      "white" : "#FFFFFF",
+      "blue" : "#3A3C66",
+      "red" : "#A54A40",
+    },
+    fontFamily: {
+      'large': [
+        '"Noto Sans"', 
+      ],
+      'small': [
+        '"Titillium Web"',
+      ]
+    },
+    width: {
+      'content': '96rem',
+    },
+    minHeight: {
+      'footer': '3rem',
+    }
+  },
+  plugins: [],
+}
+
