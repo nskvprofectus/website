a319348aafe5d4dab60d9f99788c1da025f8ccc8
Jasmijn Bookelmann <jasmijn@bookelmann.nl>
2023-12-27 22:22:47 +0100

    add html minify to minimize html code on build

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,10 @@
+// plugins
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const htmlmin = require("html-minifier");
 const fg = require('fast-glob');
 const path = require('path');
 
+// defining file paths
 const documentPaths = fg.sync(['**/media/documents/*', '!**/_site']);
 
 const documents = []
@@ -18,6 +21,19 @@ module.exports = function(config) {
 
   config.addCollection('documents', function(collection) {
     return documents;
+  });
+
+  config.addTransform("htmlmin", function(content) {
+    // Prior to Eleventy 2.0: use this.outputPath instead
+    if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+    return content;
   });
 
 
